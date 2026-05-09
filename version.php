9e9489892ca60547e9e2f9b69a86ab32bf8ade07
samehnsaman <samehnsaman@etsc.org>
2026-05-09 23:31:21 +0300

    Update local_timetable dependency version

--- version.php
+++ version.php
@@ -30,5 +30,5 @@ $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = '0.1.0';
 $plugin->dependencies = [
-    'local_timetable' => 2026042706,
+    'local_timetable' => 2026041803,
 ];
